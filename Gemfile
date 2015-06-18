404292a655e779ff7dc3642a8958d7a8e5268a2e
Daniel P. Clark <6ftdan@gmail.com>
2015-06-18 13:42:37 -0400

    Code climate

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in CardsLib.gemspec
 gemspec
+gem "codeclimate-test-reporter", group: :test, require: nil
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require 'cards_lib'
 require 'minitest/autorun'
 require 'minitest/reporters'

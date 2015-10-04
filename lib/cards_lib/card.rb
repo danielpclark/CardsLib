a5d8ab99bd00b02009144986b8932d895effaa8c
Daniel P. Clark <6ftdan@gmail.com>
2015-10-04 10:11:16 -0400

    rank and suit for cards

--- lib/cards_lib/card.rb
+++ lib/cards_lib/card.rb
@@ -2,11 +2,33 @@ module CardsLib
   class Card
     def initialize(face)
       raise InvalidCardFace, "face cannot be blank" if face.to_s.empty?
-      @face = face
+      @suit = nil
+      @rank = nil
+      if face.is_a? Hash
+        @suit = face.fetch(:suit) { nil }
+        @rank = face.fetch(:rank) { nil }
+        if @rank && @suit
+          str = ""
+          if @rank.length.>(1) && @suit.length.>(1)
+            str = " of "
+          end
+          @face = [@rank, str, @suit].join
+        end
+      else
+        @face = face
+      end
     end
 
     def face
       @face   
+    end
+
+    def suit
+      @suit || @face[1..-1]
+    end
+
+    def rank
+      @rank || @face[0]
     end
   end
 
--- test/card_test.rb
+++ test/card_test.rb
@@ -9,4 +9,25 @@ describe "Card" do
   it "Must raise InvalidCardFace for empty string" do
     -> { Card.new("") }.must_raise InvalidCardFace
   end
+
+  it "Knows its rank" do
+    card = Card.new("Ah")
+    card.rank.must_equal "A"
+  end
+
+  it "Knows its suit" do
+    card = Card.new("Ah")
+    card.suit.must_equal "h"
+  end
+
+  it "can create cards from rank and suit" do
+    card = Card.new({suit: "Hearts", rank: "Jack"})
+    card.face.must_equal "Jack of Hearts"
+    card.suit.must_equal "Hearts"
+    card.rank.must_equal "Jack"
+    card = Card.new({suit: "h", rank: "J"})
+    card.face.must_equal "Jh"
+    card.suit.must_equal "h"
+    card.rank.must_equal "J"
+  end
 end

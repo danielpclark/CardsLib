5df1b7d857b400fb96644b250f67f9997ae31598
Daniel P. Clark <6ftdan@gmail.com>
2015-11-26 20:35:10 -0500

    No need for instance variables [ci skip]

--- lib/cards_lib/is_set.rb
+++ lib/cards_lib/is_set.rb
@@ -2,9 +2,9 @@ module CardsLib
   module IsSet
     class << self
       def verify(cards, rules = [:unique, :paired], specs = {})
-        @max = specs.fetch(:max) {Float::INFINITY}
-        @min = specs.fetch(:min) { 3 }
-        rules.all? {|r| send(r, cards) } && (@min..@max).include?(cards.count)
+        max = specs.fetch(:max) {Float::INFINITY}
+        min = specs.fetch(:min) { 3 }
+        rules.all? {|r| send(r, cards) } && (min..max).include?(cards.count)
       end
 
       private 
